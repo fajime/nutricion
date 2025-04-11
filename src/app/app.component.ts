fba3cb3b6cec8e0dd5d5d0fa2318faa649442256
fajime <jm-francisco@hotmail.com>
2025-04-11 14:45:37 +0200

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'web-nutricion';
+
+  constructor( private _router:Router ) {}
+
+  goHome() {
+    this._router.navigate(['home']);
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent() {
+      return import('./components/home/home.component').then((m) => m.HomeComponent);
+    },
+  },
+  {
+    path: 'servicios',
+    loadComponent() {
+      return import('./components/servicios/servicios.component').then((m) => m.ServiciosComponent);
+    },
+  },
+  {
+    path: 'contacto',
+    loadComponent() {
+      return import('./components/contacto/contacto.component').then((m) => m.ContactoComponent);
+    },
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+];
